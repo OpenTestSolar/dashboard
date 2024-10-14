9df3c6870f6f0a164ac14c5cba5dc6008fde1d99
pinhenzhang <pinhenzhang@tencent.com>
2024-10-14 20:39:40 +0800

    完成环境变量编辑

--- dashboard-ui/src/pages/CreateTask/index.tsx
+++ dashboard-ui/src/pages/CreateTask/index.tsx
@@ -1,22 +1,33 @@
 import { memo, useState } from 'react';
-import { Input, Form, Select, SelectOption } from 'tdesign-react';
+import { Input, Form, Select, InputNumber, InputNumberValue, SelectOption } from 'tdesign-react';
 
 import './index.module.less';
 import classnames from 'classnames';
 import CommonStyle from '../../styles/common.module.less';
 import TooltipLabel from './components/TooltipLabel';
+import EnvEditor from './components/EnvEditor';
 
 const { FormItem } = Form;
 
 export const NewTask = () => {
-  const [value, setValue] = useState('');
-  const onChange = (value: string) => {
+  const [value, setValue] = useState<SelectOption>('');
+  const onChange = (value: SelectOption) => {
     setValue(value);
   };
+  const [concurrencyValue, setConcurrencyValue] = useState<InputNumberValue>(1);
+  const onConcurrencyChange = (value: InputNumberValue) => {
+    setConcurrencyValue(value);
+  };
+  const [retryCountValue, setRetryCountValue] = useState<InputNumberValue>(1);
+  const onRetryCountChange = (value: InputNumberValue) => {
+    setRetryCountValue(value);
+  };
+
+  const [envDatas, setEnvDatas] = useState();
 
   return (
     <>
-      <Form layout='vertical'>
+      <Form labelWidth={120} layout='vertical'>
         <FormItem label='任务名称' name='taskName'>
           <Input align='left' size='medium' status='default' maxlength={500} type='text' style={{ width: '200px' }} />
         </FormItem>
@@ -34,12 +45,46 @@ export const NewTask = () => {
           />
         </FormItem>
 
-        <FormItem label='LabelC:' name='inputC'>
-          <Input placeholder='请输入内容C' />
+        <FormItem label='选择用例' name='selectCase'>
+          <Input placeholder='不输入默认执行全部用例' style={{ width: '600px' }} />
         </FormItem>
 
-        <FormItem label='LabelD:' name='inputD'>
-          <Input placeholder='请输入内容D' />
+        <FormItem label='执行集群' name='inputD'>
+          <Select
+            style={{ width: '360px' }}
+            clearable
+            options={[
+              { label: '架构云', value: '1', title: '架构云选项' },
+              { label: '大数据', value: '2' },
+              { label: '区块链', value: '3' },
+            ]}
+          />
+        </FormItem>
+
+        <FormItem label='并发数目' name='concurrency'>
+          <InputNumber min={1} max={30} defaultValue={1} value={concurrencyValue} onChange={onConcurrencyChange} />
+        </FormItem>
+
+        <FormItem label='用例失败重跑次数' name='retryCount'>
+          <InputNumber min={1} max={10} defaultValue={1} value={retryCountValue} onChange={onRetryCountChange} />
+        </FormItem>
+
+        <FormItem label='测试工具' name='toolName'>
+          <Select
+            style={{ width: '120px' }}
+            clearable
+            options={[
+              { label: 'PyTest', value: 'pytest' },
+              { label: 'Ginkgo', value: 'ginkgo' },
+              { label: 'GoTest', value: 'gotest' },
+              { label: 'Jest', value: 'jest' },
+              { label: 'Playwright', value: 'playwright' },
+            ]}
+          />
+        </FormItem>
+
+        <FormItem label='环境变量' name='envs'>
+          <EnvEditor />
         </FormItem>
       </Form>
     </>
